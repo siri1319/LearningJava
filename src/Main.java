069e65f69c0d87e0b05844fe000d2d1533604bdb
Sri <siri8691@gmail.com>
2022-03-11 11:10:33 +1100

    Learned primitive datatypes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! this is me ");
+        int StudentAge = 30;
+        double StudentGpa = 5;
+        String StudentName = "Tom";
+        boolean StudentAttendance = true;
+        char StudentInitial = 'T';
+        System.out.println(StudentAge);
+        System.out.println(StudentGpa);
+        System.out.println(StudentAttendance);
+        System.out.println(StudentInitial);
+        System.out.println(StudentName);
     }
 }
