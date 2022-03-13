91936332c63ad1d05a9b146fe8b050583db40c96
Sri <siri8691@gmail.com>
2022-03-14 10:10:04 +1100

    Learned While loop

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       /* System.out.println("Hello World! this is me ");
-        int StudentAge = 3;
-        double StudentGpa = 4.5;
-        String StudentFirstName = "Tom";
-        String StudentLastName = "Hanks";
-        boolean StudentAttendance = true;
-        char StudentInitial = StudentFirstName.charAt(0);
-        System.out.println(StudentAge);
-        System.out.println(StudentGpa);
-        System.out.println(StudentAttendance);
-        System.out.println(StudentInitial);
-        System.out.println(StudentFirstName +" "+ StudentLastName +" has GPA of " + StudentGpa);
-        System.out.println("Do you want to update the Student Firstname ?");
-        Scanner input = new Scanner(System.in);
-        //StudentGpa = input.nextDouble();
-        //System.out.println("Student GPA is now "+ StudentGpa);
-        StudentFirstName = input.next();
-        System.out.println("Student Name is Updated: "+StudentFirstName +" "+StudentLastName);*/
-        System.out.println("Pick a number from 1 to 10");
-        Scanner input = new Scanner(System.in);
-        int selectedNum = input.nextInt();
-        if (selectedNum < 5)
+        System.out.println("Answer the below Question ");
+        System.out.println("What is the Capital of India?");
+        System.out.println("1.Delhi");
+        System.out.println("2.Mumbai");
+        System.out.println("3.Chennai");
+        System.out.println("4.Hyderabad");
+        boolean Option = true;
+        while (Option)
         {
-            System.out.println("Enjoy the Good Luck a Friend brings you");
+            Scanner input = new Scanner(System.in);
+            String UserInput = input.next();
+            if (UserInput.equals("Delhi"))
+            {
+                System.out.println("You have entered the Correct Option");
+                Option=false;
+            }
+            else {
+                System.out.println("You have entered the Wrong option. Please try again");
+            }
+
+
         }
-        else {
-            System.out.println("Your Shoe selection will make you happy today");
-        }
+        System.out.println("Congrats!!! You made it!");
     }
+
+
 }
