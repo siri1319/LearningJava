6aa851555b3a4ab70bd640a7592865b7dfe2c032
Sri <siri8691@gmail.com>
2022-03-15 18:08:34 +1100

    Learned functions with parameters (Calculate the % of student marks)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import java.util.Locale;
 import java.util.Scanner;
 public class Main {
-    public static void goodMorning(){
-        System.out.println("Good Morning..!! please enter any number from 1 to 5 to try your luck!");
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        if (number == 3){
-            System.out.println("You are the Luckiest person today!!!");
-            }
-        else
-        {
-            System.out.println("You are not lucky!!! Try your luck tomorrow!!! ");
-        }
+    public static void percentageMarks(int maths1,int science1, int computers1){
+
+        double TotalMarksScored = maths1+science1+computers1;
+        double TotalMarks = 75;
+        double percentage = (TotalMarksScored/TotalMarks)*100;
+        System.out.println("Percentage Scored by student :: " +percentage);
     }
     public static void main(String[] args) {
-        System.out.println("Welcome to the Job! ");
-        goodMorning();
+        System.out.println("Welcome to the Student Portal! ");
+        System.out.println("Enter student marks:");
+        System.out.println("Enter marks scored in maths out of 25");
+        Scanner input = new Scanner(System.in);
+        int maths = input.nextInt();
+        System.out.println("Enter marks scored in Science out of 25");
+        int science = input.nextInt();
+        System.out.println("Enter marks scored in Computers out of 25");
+        int computers = input.nextInt();
+        percentageMarks(maths,science,computers);
 
         System.out.println("All the best for your rest of the day. Thank you!");
 
