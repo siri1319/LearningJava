99d4aaed8a33151fce8b10680319b4d7cf0f49e6
Sri <siri8691@gmail.com>
2022-03-21 16:39:28 +1100

    Learned class and created a sample student profile to understand the concept

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,14 @@
 import java.util.Locale;
 import java.util.Scanner;
 public class Main {
-    public static double employeeSalary(double hoursPerWeek, double payPerHour){
 
-        double yearlySalary = 52 * hoursPerWeek * payPerHour;
-        return yearlySalary;
-    }
-    public static double vaccation(double unpaiddays, double payPerHour){
-
-        double unpaidSalary = 8 * unpaiddays * payPerHour;
-        return unpaidSalary;
-    }
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee Portal!  ");
-        System.out.println("Enter employee's work hours per week :");
-        Scanner input = new Scanner(System.in);
-        double hours = input.nextDouble();
-        System.out.println("Enter employee's pay per hour");
-        double pay = input.nextDouble();
-        double yearSalary = employeeSalary(hours,pay);
+        StudentProfile StudentA = new StudentProfile("Tom","Hanks",3.0,2012,"Information Technology");
+        StudentProfile StudentB = new StudentProfile("Tony","Stark",4.3,2001,"Artificial Intelligence");
 
-        System.out.println("Enter employee's No. of vaccation days :");
-        double numberofdays = input.nextDouble();
-
-        double unpaid = vaccation(numberofdays,pay);
-
-        double TotalEmpSal = yearSalary - unpaid;
-
-        System.out.println("Employee Salary is ::" +TotalEmpSal);
-
+        System.out.println(StudentA.StudentFirstName +" "+ StudentA.StudentLastName);
+        int incrementedgraduationyear = StudentProfile.IncrementGraduationYear(StudentA.GraduationYear);
+        System.out.println("Incremented graduation year for Student A"+"  " +incrementedgraduationyear);
     }
 
 
