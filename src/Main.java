1e65524dca5dda69b36ac93591306ab4dcc1daa5
Sri <siri8691@gmail.com>
2022-03-22 14:13:11 +1100

    changed code in student profile

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        StudentProfile StudentA = new StudentProfile("Tom","Hanks",3.0,2012,"Information Technology");
+        StudentProfile StudentA = new StudentProfile("Tom","Hanks",3.0,2017,"Information Technology");
         StudentProfile StudentB = new StudentProfile("Tony","Stark",4.3,2001,"Artificial Intelligence");
 
         System.out.println(StudentA.StudentFirstName +" "+ StudentA.StudentLastName);
-        int incrementedgraduationyear = StudentProfile.IncrementGraduationYear(StudentA.GraduationYear);
-        System.out.println("Incremented graduation year for Student A"+"  " +incrementedgraduationyear);
+
+        StudentA.IncrementGraduationYear();
+        System.out.println("Student A graduation year" + StudentA.GraduationYear);
     }
 
 
